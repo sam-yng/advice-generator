194db16d1d6f7d188423dde109a74713be24c393
sam-yng <young142001@hotmail.com>
2023-05-10 12:20:35 +1000

    final, with shadow bug

--- src/components/AdviceBox.tsx
+++ src/components/AdviceBox.tsx
@@ -20,23 +20,27 @@ const AdviceBox = () => {
   const adviceText = Object.values(advice);
 
   return (
-    <main className="rounded-xl text-white w-[40vw] bg-[#313A49]">
-      <div className="flex m-10 text-center flex-col">
-        <h2 className="text-[#53FFAB] tracking-widest text-[16px] mt-2">
-          ADVICE #{adviceText[0]}
-        </h2>
-        <h1 className="text-[#CFE3EA] tracking-wider text-[26px] mt-6">
-          &quot;{adviceText[1]}&quot;
-        </h1>
-        <img className="object-none object-bottom" src={divider} />
+    <div className="flex flex-col">
+      <main className="rounded-xl text-white w-[40vw] bg-[#313A49]">
+        <div className="flex m-10 text-center flex-col">
+          <h2 className="text-[#53FFAB] tracking-widest text-[16px] mt-2">
+            ADVICE #{adviceText[0]}
+          </h2>
+          <h1 className="text-[#CFE3EA] tracking-wider text-[26px] mt-6">
+            &quot;{adviceText[1]}&quot;
+          </h1>
+        </div>
+        <img className="w-[75%] m-auto pb-16" src={divider} />
+      </main>
+      <div className="flex justify-center -mt-7">
+        <button
+          onClick={fetchData}
+          className="border-2 hover:shadow-md hover:shadow-[#53FFAB] border-[#53FFAB] w-14 h-14 bg-[#53FFAB] rounded-full"
+        >
+          <img className="m-auto" src={dice} />
+        </button>
       </div>
-      <button
-        onClick={fetchData}
-        className="border-2 border-[#53FFAB] w-14 h-14 bg-[#53FFAB] rounded-full"
-      >
-        <img className="m-auto" src={dice} />
-      </button>
-    </main>
+    </div>
   );
 };
 
